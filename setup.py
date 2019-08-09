5ea9fd968b506f705239605ee0e89a8430f4c2e2
Jan Betley <jan.betley@zweryfikujfirme.pl>
2019-08-09 14:57:29 +0200

    v0.005 - sorting and limiting GET collections
    Currently implemented only in the Engine layer, not in the storages,
    so there is no performance increase (still all instances are created
    and only later limited), but future storage-side implementation
    will be invisible from the API point of view.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='blargh',  
-    version='0.0.4',
+    version='0.0.5',
     author='Jan Betley',
     author_email='jan.betley@gmail.com',
     description='Generic REST API base',
